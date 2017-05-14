63d90e7a4d4300d7da4a53444e51fe75a1dfe818
przemyslawmoskal <ptmoskal@gmail.com>
2017-05-14 23:25:43 +0200

    Add Chapter 02 Exercise 02

---
+++ Chapter 02 - Everything is an Object/Exercise 02/ch02ex02/Ch02ex02.java
@@ -0,0 +1,13 @@
+package ch02ex02;
+
+public class Ch02ex02 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+
+}
+
+/* Output:
+Hello world!
+*/
---
+++ Chapter 02 - Everything is an Object/Exercise 02/readme.txt
@@ -0,0 +1,3 @@
+Following the HelloDate.java example in this chapter, create a "hello, world" program that simply displays that statement.
+
+Chapter 02 Exercise 02
