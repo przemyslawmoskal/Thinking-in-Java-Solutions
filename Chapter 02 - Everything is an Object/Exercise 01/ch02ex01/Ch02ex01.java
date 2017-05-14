aa0e5d1c1bf1f4047f6e7bff558d77bf18f50cf9
przemyslawmoskal <ptmoskal@gmail.com>
2017-05-14 23:24:28 +0200

    Add Chapter 02 Exercise 01

---
+++ Chapter 02 - Everything is an Object/Exercise 01/ch02ex01/Ch02ex01.java
@@ -0,0 +1,17 @@
+package ch02ex01;
+
+public class Ch02ex01 {
+	int i;
+	char c;
+	public static void main(String[] args) {
+		Ch02ex01 obj = new Ch02ex01();
+		System.out.println("int i = " + obj.i);
+		System.out.println("char c = " + obj.c);
+	}
+
+}
+
+/* Output:
+int i = 0
+char c = 
+*/
---
+++ Chapter 02 - Everything is an Object/Exercise 01/readme.txt
@@ -0,0 +1,4 @@
+Create a class containing an int and a char that are not intitialized
+and print their values to verify that Java performs default initialization.
+
+Chapter 02 Exercise 01
