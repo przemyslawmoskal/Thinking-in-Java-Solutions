26e1d7c681d5fea6d2adb9b7bdf78408d1019cd6
przemyslawmoskal <ptmoskal@gmail.com>
2017-05-14 23:32:47 +0200

    Add Chapter 02 Exercise 10

---
+++ Chapter 02 - Everything is an Object/Exercise 10/ch02ex10/Ch02ex10.java
@@ -0,0 +1,17 @@
+package ch02ex10;
+
+public class Ch02ex10 {
+
+	public static void main(String[] args) {
+		System.out.println("args[0] = " + args[0]);
+		System.out.println("args[1] = " + args[1]);
+		System.out.println("args[2] = " + args[2]);
+	}
+
+}
+
+/* Output (example):
+args[0] = a
+args[1] = b
+args[2] = c
+*/
---
+++ Chapter 02 - Everything is an Object/Exercise 10/readme.txt
@@ -0,0 +1,4 @@
+Write a program that prints three arguments taken from the command line.
+To do this you'll need to index into the command-line array of Strings.
+
+Chapter 02 Exercise 10
