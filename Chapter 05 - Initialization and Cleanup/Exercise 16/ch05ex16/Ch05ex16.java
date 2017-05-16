4c0a7bd967be0aff8effe3d87ad048efc0655066
przemyslawmoskal <ptmoskal@gmail.com>
2017-05-16 23:24:39 +0200

    Add Chapter 05 Exercise 16

---
+++ Chapter 05 - Initialization and Cleanup/Exercise 16/ch05ex16/Ch05ex16.java
@@ -0,0 +1,17 @@
+package ch05ex16;
+
+public class Ch05ex16 {
+	
+    public static void main(String[] args) {
+        String[] arr = new String[]{"abc","def","ghi",};
+        for(int i=0; i<arr.length; i++){
+            System.out.println("arr["+i+"] = " + arr[i]);
+        }
+    } 
+}
+
+/* Output:
+arr[0] = abc
+arr[1] = def
+arr[2] = ghi
+*/
---
+++ Chapter 05 - Initialization and Cleanup/Exercise 16/readme.txt
@@ -0,0 +1,4 @@
+Create an array of String objects and assign a String to each element.
+Print the array by using a for loop.
+
+Chapter 05 Exercise 16
