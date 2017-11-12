2eb5016d6873fbc80949ff0e9806c66633ab66cf
Przemysław Moskal <28692052+przemyslawmoskal@users.noreply.github.com>
2017-11-12 23:20:41 +0100

    Update of Packages imported in exercises folder

--- Packages imported in exercises/net/mindview/util/Tuple.java
+++ Packages imported in exercises/net/mindview/util/Tuple.java
@@ -18,8 +18,4 @@ public class Tuple {
   FiveTuple<A,B,C,D,E> tuple(A a, B b, C c, D d, E e) {
     return new FiveTuple<A,B,C,D,E>(a, b, c, d, e);
   }
-  public static <A,B,C,D,E,F>
-  SixTuple<A,B,C,D,E,F> tuple(A a, B b, C c, D d, E e, F f) {
-    return new SixTuple<A,B,C,D,E,F>(a, b, c, d, e, f);
-  }
 } ///:~
