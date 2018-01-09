5449b7b61ea6b3bd7919c049ab4d0f4a43567f1a
Przemysław Moskal <ptmoskal@gmail.com>
2018-01-09 22:42:53 +0100

    Add Chapter 22 Exercise 33

---
+++ Chapter 22 - Graphical User Interfaces/Exercise 33/net/mindview/util/TaskManager.java
@@ -0,0 +1,45 @@
+// Managing and executing a queue of tasks.
+package net.mindview.util;
+import java.util.concurrent.*;
+import java.util.*;
+
+public class TaskManager<R, C extends Callable<R>> extends ArrayList<TaskItem<R, C>> {
+	// Only this line changed:
+	private ExecutorService exec = Executors.newCachedThreadPool();
+
+	public void add(C task) {
+		add(new TaskItem<R, C>(exec.submit(task), task));
+	}
+
+	public List<R> getResults() {
+		Iterator<TaskItem<R, C>> items = iterator();
+		List<R> results = new ArrayList<R>();
+		while (items.hasNext()) {
+			TaskItem<R, C> item = items.next();
+			if (item.future.isDone()) {
+				try {
+					results.add(item.future.get());
+				} catch (Exception e) {
+					throw new RuntimeException(e);
+				}
+				items.remove();
+			}
+		}
+		return results;
+	}
+
+	public List<String> purge() {
+		Iterator<TaskItem<R, C>> items = iterator();
+		List<String> results = new ArrayList<String>();
+		while (items.hasNext()) {
+			TaskItem<R, C> item = items.next();
+			// Leave completed tasks for results reporting:
+			if (!item.future.isDone()) {
+				results.add("Cancelling " + item.task);
+				item.future.cancel(true); // May interrupt
+				items.remove();
+			}
+		}
+		return results;
+	}
+}
---
+++ Chapter 22 - Graphical User Interfaces/Exercise 33/readme.txt
@@ -0,0 +1,4 @@
+Modify InterruptableLongRunningCallable.java so that it runs
+all the tasks in parallel rather than sequentially.
+
+Chapter 22 Exercise 33
